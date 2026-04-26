c61aa67bfb38f18ed4b7e2f9572ac7a4d30d7b99
Omkar K P <70146112+Keerthiomkar@users.noreply.github.com>
2026-04-26 11:26:33 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -8,4 +8,4 @@ python manage.py collectstatic --noinput
 
 # Start the Gunicorn server
 PORT=${PORT:-8000}  # Use dynamic PORT if set, default to 8000
-exec gunicorn myproject.wsgi:application --bind 0.0.0.0:$PORT --workers 3
+exec gunicorn attendance_system.wsgi:application --bind 0.0.0.0:$PORT --workers 3
