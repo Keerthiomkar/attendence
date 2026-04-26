c7367e18bd5853cd8a245d184125581c80733998
Omkar K P <70146112+Keerthiomkar@users.noreply.github.com>
2026-04-26 10:58:37 +0530

    Update ALLOWED_HOSTS to use a safe default

--- attendance_system/settings.py
+++ attendance_system/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY
 SECRET_KEY = os.environ.get('SECRET_KEY', 'unsafe-secret-key')
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
 
 # APPLICATIONS
 INSTALLED_APPS = [
